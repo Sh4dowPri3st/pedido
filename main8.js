8c065d9d11fee59485b1c32e4343e0e2bc1abb9d
Lautaro Bertera <bertera.lautaro@gmail.com>
2023-01-22 18:25:26 -0300

    PEDIDO 1

---
+++ main8.js
@@ -0,0 +1,16 @@
+//Funciones que retornan otra funcion
+
+//Creamos la funcion
+// function mayorQue(n){
+//     return (m) => m > n 
+// }
+
+// //declaramos
+// let mayorQueDoce = mayorQue (12)
+// // meyorQueDoce es (m) => m > 12
+
+// //la usamos
+// console.log(mayorQueDoce(25))
+// //true
+
+alert("ESTA LISTA SU ORDEN!!!")
